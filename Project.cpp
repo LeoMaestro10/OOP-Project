c2cd352a7bf6e274f46b43703832169fc851d48a
abdul moiez <moiezabdul45@gmail.com>
2023-12-11 13:03:12 +0500

    Added Classes

--- Project.cpp
+++ Project.cpp
@@ -1,5 +1,56 @@
 #include<iostream>
 using namespace std;
+class Student
+{
+	string Name;
+	string roll_No;
+	int Age;
+	long double contact;
+	string Courses;
+	char Attendance[];
+	int marks;
+
+private:
+
+};
+class Course
+{
+	string course_Code;
+	string course_Name;
+	string course_Instructor;
+	int course_Credits;
+	double course_Capacity;
+	int course_Students;
+
+};
+
+class System
+{
+	double Students;
+	int Courses;
+	int Menus;
+
+};
+
+class FileHandler
+{
+	string FileName;
+
+
+};
+
+class Validator
+{
+public:
+	
+
+private:
+
+};
+
+
+
+
 
 
 void main()
